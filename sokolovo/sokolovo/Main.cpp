b2eb14ed40abf0e58ba4ee52d001dbf27ca1e4c1
Henry Chronowski <hrchronowski@gmail.com>
2023-07-27 10:45:57 -0400

    Adding initial glfw setup

--- sokolovo/sokolovo/Main.cpp
+++ sokolovo/sokolovo/Main.cpp
@@ -1,6 +1,23 @@
-#include <glad/glad.h>
+#include <iostream>
+#include <cassert>
+
+#include <glad/glad.h>	// GLAD must be included before any other includes that use OpenGL
+#include <GLFW/glfw3.h>
 
 int main()
 {
-	return 0;
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Learn OpenGL", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window\n";
+		glfwTerminate();
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
 }
